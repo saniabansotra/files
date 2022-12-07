d9bb0d764e7bfd6c07409feef69b21c5b69f45b4
Sania Bansotra <saniabansotra@gmail.com>
2022-12-08 00:21:12 +0530

    message

---
+++ script.js
@@ -0,0 +1,43 @@
+//Day 1
+console.log("Hello World");
+console.log("Hyy this is Sania");
+
+
+//Day 2
+let a=2;
+let b=5;
+console.log("Sum is :",  a+b);
+console.log("Sum is :",  a-b);
+console.log("Sum is :",  a*b);
+console.log("Sum is :",  a/b);
+const c=34;//integer
+a=56;
+console.log(a);
+console.log(c);
+d="sania";//string
+console.log(d);
+e=true;//boolean
+console.log(e);
+f=0.36483//float
+console.log(f);
+g="Sania";
+h=" Bansotra ";
+console.log(g+h);//concatination
+i="Hello! I am studying in MMMUT Gorakhpur";
+console.log(i.length);
+console.log(i.toLowerCase());
+console.log(i.toUpperCase());
+//slice
+//split
+//charAt
+g="Hello! This is JavaScript Tutorial";
+console.log(g.slice(6));
+console.log(g.split("is"));
+console.log(g.charAt("10"));
+
+
+
+
+
+
+
