14d1bb090fd81653898df793371930c790cd844b
Sania Bansotra <saniabansotra@gmail.com>
2022-12-14 19:47:16 +0530

    message

---
+++ array.js
@@ -0,0 +1,33 @@
+//To store multiple values in single variable, we need array.
+
+// Array is the most used datatype in the history of Web development after Objects.
+
+// array is declared just like any other variable but enclosed within square brackets.
+
+// A particular array in javascript can contain any kind of datatype.
+// example  let a = ["Sania", {}, null, true, 5];
+
+
+let a = ["Post 1", "Post 2", "Post 3", "Post 4", "Post 5"];
+
+// acceessing values of array
+// console.log(a[0]);
+// console.log(a[1]);
+// console.log(a[2]);
+// console.log(a[3]);
+// console.log(a[4]);
+
+// getting length of array
+// console.log(a.length);
+
+// adding an element in array
+// a.push("Post 6");
+// a.push("Post 7");
+// a.push("Post 8");
+
+// removing an element:-
+// a.pop();     removes last element
+console.log(a);
+for( index=0;index<5;index++){
+    console.log(a[index]);
+}
---
+++ loop.js
@@ -0,0 +1,27 @@
+// for loop
+// for(let x=2;x<=8;x++)
+// {
+//     console.log(x);
+// }
+// infinite for loop
+// for(let y=1;y>0;y++)
+// for(let y=1;;y++)
+// {
+// console.log("happy");
+// }
+// program to display the sum of n natural numbers
+// let x=1;
+// const y=100;
+// let sum=0;
+// for(;x<=y;x++)
+// {
+// sum+=x;
+
+// }
+// console.log(sum);
+var x = 10;
+        for ( ; ; x++)
+        {
+            console.log("Value of x is :" + x);
+            break;
+        }
