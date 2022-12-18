db8b958ee93edc78e0a59d725004201911af2abe
Sania Bansotra <saniabansotra@gmail.com>
2022-12-18 20:20:05 +0530

    message

---
+++ for_loop2.js
@@ -0,0 +1,41 @@
+// for(let i=0;i<10;i++){
+//     console.log(i);
+// }
+// for(let index=4;index<=50;index=index*3){
+    
+//         console.log(index);
+       
+//         }
+
+//     break statement
+
+// for(let i=0;i<40;i++){
+//     if(i===30){
+//         break;
+//     }
+//     console.log(i);
+// }
+// console.log("I am outside the loop:");
+
+
+
+// continue statement
+// for(let i=0;i<40;i++){
+//     if(i===30){
+//         continue;
+//     }
+//     console.log("hello" + i);
+    
+// }
+// console.log("hello this is saNIA ");
+
+
+// FOR OF LOOP
+const array=["ram","Sham", "Tarun", "Mohan","Sharddha"];
+for(let a of array){
+    console.log(a)
+}
+//The variable a starts from first element of array and goes to last by updating its value to next el3ement one by one.
+
+
+  
